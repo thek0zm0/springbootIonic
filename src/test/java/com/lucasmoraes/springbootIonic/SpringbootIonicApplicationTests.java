4ee58f15e7cd9921e84f5ddfe15f326ad4ad67c1
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-06-04 18:51:02 -0300

    Project Created

---
+++ src/test/java/com/lucasmoraes/springbootIonic/SpringbootIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraes.springbootIonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
