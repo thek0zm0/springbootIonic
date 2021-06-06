cabe4e0c1e15528d1df1947f784f47e8a3413698
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-06-06 08:08:09 -0300

    Instantiating category

--- src/main/java/com/lucasmoraes/springbootIonic/SpringbootIonicApplication.java
+++ src/main/java/com/lucasmoraes/springbootIonic/SpringbootIonicApplication.java
@@ -1,13 +1,30 @@
 package com.lucasmoraes.springbootIonic;
 
+import com.lucasmoraes.springbootIonic.domain.Category;
+import com.lucasmoraes.springbootIonic.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringbootIonicApplication {
+public class SpringbootIonicApplication implements CommandLineRunner
+{
+	@Autowired
+	private CategoryRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception
+	{
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		repository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
