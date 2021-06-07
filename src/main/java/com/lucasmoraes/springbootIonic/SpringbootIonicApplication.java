6d5ee48fc13d5e48c226b65ebbaf40f255ee2b4c
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-06-07 05:53:22 -0300

    Entities City and State

--- src/main/java/com/lucasmoraes/springbootIonic/SpringbootIonicApplication.java
+++ src/main/java/com/lucasmoraes/springbootIonic/SpringbootIonicApplication.java
@@ -1,9 +1,13 @@
 package com.lucasmoraes.springbootIonic;
 
 import com.lucasmoraes.springbootIonic.domain.Category;
+import com.lucasmoraes.springbootIonic.domain.City;
 import com.lucasmoraes.springbootIonic.domain.Product;
+import com.lucasmoraes.springbootIonic.domain.State;
 import com.lucasmoraes.springbootIonic.repositories.CategoryRepository;
+import com.lucasmoraes.springbootIonic.repositories.CityRepository;
 import com.lucasmoraes.springbootIonic.repositories.ProductRepository;
+import com.lucasmoraes.springbootIonic.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +24,12 @@ public class SpringbootIonicApplication implements CommandLineRunner
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private StateRepository stateRepository;
+
+	@Autowired
+	private CityRepository cityRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicApplication.class, args);
 	}
@@ -34,7 +44,6 @@ public class SpringbootIonicApplication implements CommandLineRunner
 		Product p2 = new Product(null,"Impressora", 800.0);
 		Product p3 = new Product(null,"Mouse", 80.0);
 
-
 		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
 		productRepository.flush();
@@ -43,12 +52,29 @@ public class SpringbootIonicApplication implements CommandLineRunner
 
 		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
-
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p2.getCategories().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 
 		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+
+		City ct1 = new City(null, "Uberlândia", st1);
+		City ct2 = new City( null, "São Paulo", st2);
+		City ct3 = new City(null, "Campinas", st2);
+
+		stateRepository.saveAll(Arrays.asList(st1,st2));
+		cityRepository.saveAll(Arrays.asList(ct1,ct2,ct3));
+		cityRepository.flush();
+		stateRepository.flush();
+
+		st1.getCities().addAll(Arrays.asList(ct1));
+		st2.getCities().addAll(Arrays.asList(ct2,ct3));
+
+		stateRepository.saveAll(Arrays.asList(st1,st2));
+		cityRepository.saveAll(Arrays.asList(ct1,ct2,ct3));
 	}
 }
