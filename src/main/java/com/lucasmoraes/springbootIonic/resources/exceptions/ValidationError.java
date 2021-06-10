98f91db3a10081356fb25bd1a7faf233d6f81600
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-06-10 08:27:08 -0300

    Personalized error message

---
+++ src/main/java/com/lucasmoraes/springbootIonic/resources/exceptions/ValidationError.java
@@ -0,0 +1,27 @@
+package com.lucasmoraes.springbootIonic.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError
+{
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp)
+    {
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors()
+    {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message)
+    {
+        FieldMessage fieldMessage = new FieldMessage(fieldName,message);
+        errors.add(fieldMessage);
+    }
+}
